bd445f6d6d520df5d71b4155e8929e8e69551352
Kerosene2000 <renault.cle@gmail.com>
2015-10-18 00:30:37 +0200

    working good csv reader

--- src/bin/trainer.rs
+++ src/bin/trainer.rs
@@ -1,3 +1,17 @@
+extern crate csv;
+
 fn main() {
-    println!("Hello, trainer!");
+
+  if let Some(file) = std::env::args().nth(1) {
+
+    let mut rdr = csv::Reader::from_file(file).unwrap().has_headers(true);
+    for row in rdr.decode() {
+
+        let (km, price): (u32, u32) = row.unwrap();
+        println!("{}, {}", km, price);
+    }
+  }
+  else {
+    panic!("You need to specify a `csv file`. format: (km,price)");
+  }
 }
