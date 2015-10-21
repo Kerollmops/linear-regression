aa5e633731d2bfbe2f29a272a2c24127e360667d
Kerosene2000 <renault.cle@gmail.com>
2015-10-21 17:04:22 +0200

    working good !

--- src/bin/estimator.rs
+++ src/bin/estimator.rs
@@ -1,5 +1,6 @@
 extern crate csv;
 use std::io::{self, Write};
+use std::f32;
 mod common;
 
 fn ask_mileage() -> u32 {
@@ -24,7 +25,7 @@ pub fn retrieve_thetas(file: &str) -> Option<(f32, f32)> {
     None
 }
 
-pub fn retrieve_min_max(file: &str) -> Option<(u32, u32)> {
+pub fn retrieve_min_max(file: &str) -> Option<(f32, f32)> {
 
     if let Ok(mut rdr) = csv::Reader::from_file(file) {
 
@@ -44,9 +45,9 @@ fn main() {
 
     // retrieving data
     let (theta0, theta1) = retrieve_thetas(thetas_file.as_ref()).unwrap_or((0_f32, 0_f32));
-    let (min, max) = retrieve_min_max(common::TMP_FILE_MIN_MAX.unwrap_or((0_u32, 0_u32));
-    let norm_mileage = common::normalize(ask_mileage() as f32, min, max);
+    let (min, max) = retrieve_min_max(common::TMP_FILE_MIN_MAX).unwrap_or((0_f32, f32::INFINITY));
+    let normed_mileage = common::normalize(ask_mileage() as f32, min, max);
 
-    let estimated = common::estimate_price(norm_mileage, theta0, theta1);
+    let estimated = common::estimate_price(normed_mileage, theta0, theta1);
     println!("Estimated price is `{}`", estimated);
 }
--- src/bin/trainer.rs
+++ src/bin/trainer.rs
@@ -5,9 +5,8 @@ fn train_thetas(learn_rate: f32, theta0: f32, theta1: f32,
                 norm_data: &Vec<(f32, u32)>) -> (f32, f32) {
 
     let m = norm_data.len() as f32;
-
-    let mut sum0 = 0.0_f32;
-    let mut sum1 = 0.0_f32;
+    let mut sum0 = 0_f32;
+    let mut sum1 = 0_f32;
 
     for &(miles, price) in norm_data {
 
@@ -15,7 +14,6 @@ fn train_thetas(learn_rate: f32, theta0: f32, theta1: f32,
         sum0 += d;
         sum1 += d * miles as f32;
     }
-    // println!("m: {}, sum0: {}, sum1: {}", m, sum0, sum1);
     (learn_rate * (1_f32 / m) * sum0, learn_rate * (1_f32 / m) * sum1)
 }
 
@@ -29,10 +27,6 @@ fn loop_train_thetas(learn_rate: f32, theta0: f32, theta1: f32,
     for _ in 0..50_000 {
 
         let (tmp_theta0, tmp_theta1) = train_thetas(learn_rate, theta0, theta1, &norm_data);
-
-        // println!("theta0: {}, theta1: {}", theta0, theta1);
-        // println!("tmp_theta0: {}, tmp_theta1: {}", tmp_theta0, tmp_theta1);
-
         theta0 -= tmp_theta0;
         theta1 -= tmp_theta1;
     }
@@ -50,9 +44,20 @@ fn save_thetas(file: &str, theta0: f32, theta1: f32) {
     assert!(result.is_ok());
 }
 
+fn save_min_max(file: &str, min: f32, max: f32) {
+
+    let mut wtr = csv::Writer::from_file(file).unwrap();
+
+    let result = wtr.encode(("min", "max"));
+    assert!(result.is_ok());
+
+    let result = wtr.encode((min, max));
+    assert!(result.is_ok());
+}
+
 fn min_tuple0(data: &Vec<(u32, u32)>) -> u32 {
 
-    let mut ret = u32::max_value(); // 0 haha !
+    let mut ret = u32::max_value();
     for &(val, _) in data {
         if val < ret {
             ret = val;
@@ -63,7 +68,7 @@ fn min_tuple0(data: &Vec<(u32, u32)>) -> u32 {
 
 fn max_tuple0(data: &Vec<(u32, u32)>) -> u32 {
 
-    let mut ret = u32::min_value(); // 0 haha !
+    let mut ret = u32::min_value();
     for &(val, _) in data {
         if val > ret {
             ret = val;
@@ -72,11 +77,11 @@ fn max_tuple0(data: &Vec<(u32, u32)>) -> u32 {
     ret
 }
 
-fn normalize_data(data: &Vec<(u32, u32)>, out: &mut Vec<(f32, u32)>, min_max: (u32, u32)) {
+fn normalize_data(data: &Vec<(u32, u32)>, out: &mut Vec<(f32, u32)>, min_max: (f32, f32)) {
 
     // let min = min_tuple0(data) as f32;
     // let max = max_tuple0(data) as f32;
-    let (min, max) = min_max as (f32, f32);
+    let (min, max) = min_max;
     out.clear();
     for &(miles, price) in data {
         out.push( (common::normalize(miles as f32, min, max), price) );
@@ -91,9 +96,9 @@ fn main() {
         let mut rdr = csv::Reader::from_file(filename).unwrap();
         let data = rdr.decode().collect::<csv::Result<Vec<(u32, u32)>>>().unwrap();
 
-        let min_max = (min_tuple0(data), max_tuple0(data));
-        let mut norm_data: Vec<(f32, u32)> = Vec::with_capacity(data.len(), min_max);
-        normalize_data(&data, &mut norm_data);
+        let min_max = (min_tuple0(&data) as f32, max_tuple0(&data) as f32);
+        let mut norm_data: Vec<(f32, u32)> = Vec::with_capacity(data.len());
+        normalize_data(&data, &mut norm_data, min_max);
 
         let learn_rate = 0.1_f32;
         let (theta0, theta1) = (0_f32, 0_f32);
@@ -102,6 +107,7 @@ fn main() {
         println!("After computation: ({}, {})", trained_theta0, trained_theta1);
 
         save_thetas(common::TMP_FILE_THETAS, trained_theta0, trained_theta1);
+        save_min_max(common::TMP_FILE_MIN_MAX, min_max.0, min_max.1);
     }
     else {
         panic!("You need to specify a `csv file`. format: (miles,price)");
