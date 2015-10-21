ee9a0eb4e5f834831aa17174426f534a144aadbd
Kerosene2000 <renault.cle@gmail.com>
2015-10-21 17:33:45 +0200

    new options

--- src/bin/trainer.rs
+++ src/bin/trainer.rs
@@ -1,4 +1,6 @@
 extern crate csv;
+extern crate getopts;
+use getopts::Options;
 mod common;
 
 fn train_thetas(learn_rate: f32, theta0: f32, theta1: f32,
@@ -88,28 +90,51 @@ fn normalize_data(data: &Vec<(u32, u32)>, out: &mut Vec<(f32, u32)>, min_max: (f
     }
 }
 
+fn print_usage(program: &str, opts: Options) {
+
+    let brief = format!("Usage: {} -d DATASET [options]", program);
+    print!("{}", opts.usage(&brief));
+}
+
 // https://github.com/SiegeLord/RustGnuplot
 fn main() {
 
-    if let Some(filename) = std::env::args().nth(1) {
+    let args: Vec<String> = std::env::args().collect();
+    let program = args[0].clone();
 
-        let mut rdr = csv::Reader::from_file(filename).unwrap();
-        let data = rdr.decode().collect::<csv::Result<Vec<(u32, u32)>>>().unwrap();
+    let mut opts = Options::new();
+    opts.reqopt("d", "dataset", "the file containing the dataset you want to read", "DATASET");
+    opts.optopt("l", "learning-rate", "set the learning rate. default is 0.1", "VALUE");
+    opts.optopt("", "tmp-file-thetas", format!("set the file where you want to save thetas values, default {}", common::TMP_FILE_THETAS).as_ref(), "FILE");
+    opts.optopt("", "tmp-file-min-max", format!("set the file where you want to save min-max values, default {}", common::TMP_FILE_MIN_MAX).as_ref(), "FILE");
+    opts.optflag("g", "graph", "display a graph representing the data and the curve found");
+    opts.optflag("h", "help", "print this help menu");
+    let matches = match opts.parse(&args[1..]) {
+        Ok(m) => { m }
+        Err(f) => { panic!(f.to_string()) }
+    };
 
-        let min_max = (min_tuple0(&data) as f32, max_tuple0(&data) as f32);
-        let mut norm_data: Vec<(f32, u32)> = Vec::with_capacity(data.len());
-        normalize_data(&data, &mut norm_data, min_max);
-
-        let learn_rate = 0.1_f32;
-        let (theta0, theta1) = (0_f32, 0_f32);
-
-        let (trained_theta0, trained_theta1) = loop_train_thetas(learn_rate, theta0, theta1, &norm_data);
-        println!("After computation: ({}, {})", trained_theta0, trained_theta1);
-
-        save_thetas(common::TMP_FILE_THETAS, trained_theta0, trained_theta1);
-        save_min_max(common::TMP_FILE_MIN_MAX, min_max.0, min_max.1);
+    if matches.opt_present("h") {
+        print_usage(&program, opts);
+        return;
     }
-    else {
-        panic!("You need to specify a `csv file`. format: (miles,price)");
-    }
+
+    let filename = matches.opt_str("d").unwrap();
+    let mut rdr = csv::Reader::from_file(filename).unwrap();
+    let data = rdr.decode().collect::<csv::Result<Vec<(u32, u32)>>>().unwrap();
+
+    let min_max = (min_tuple0(&data) as f32, max_tuple0(&data) as f32);
+    let mut norm_data: Vec<(f32, u32)> = Vec::with_capacity(data.len());
+    normalize_data(&data, &mut norm_data, min_max);
+
+    let learn_rate: f32 = matches.opt_str("l").unwrap_or("0.1".to_string()).parse().ok().expect("We want a number...");
+    let (theta0, theta1) = (0_f32, 0_f32);
+
+    let (trained_theta0, trained_theta1) = loop_train_thetas(learn_rate, theta0, theta1, &norm_data);
+    println!("After computation: ({}, {})", trained_theta0, trained_theta1);
+
+    let tmp_file_thetas = matches.opt_str("tmp-file-thetas").unwrap_or(common::TMP_FILE_THETAS.to_string());
+    let tmp_file_min_max = matches.opt_str("tmp-file-min-max").unwrap_or(common::TMP_FILE_MIN_MAX.to_string());
+    save_thetas(tmp_file_thetas.as_ref(), trained_theta0, trained_theta1);
+    save_min_max(tmp_file_min_max.as_ref(), min_max.0, min_max.1);
 }
